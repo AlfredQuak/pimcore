3a2662458926f9b065b66057db152119d606e4e6
daniel.stecker <daniel.stecker@wolterskluwer.com>
2022-03-29 13:43:59 +0200

    skip-database-setup

--- bundles/InstallBundle/Command/InstallCommand.php
+++ bundles/InstallBundle/Command/InstallCommand.php
@@ -324,7 +324,7 @@ class InstallCommand extends Command
         $params = [];
         $missing = [];
 
-        echo var_dump($this->getOptions());
+        echo var_dump($this);
 
         if(!in_array("skipDatabaseConfig", $this->getOptions(), true)){
             foreach ($this->getOptions() as $name => $config) {
